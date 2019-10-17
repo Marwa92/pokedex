ff0105afee5a554067ccc802fadc5eb5b7fa2b30
Marwa Yahia Galal <engmarwayehya@yahoo.com>
2019-10-18 00:31:46 +0200

    test

--- precache-manifest.aedccdda9aa967e3f0732b10564ff71a.js
+++ precache-manifest.aedccdda9aa967e3f0732b10564ff71a.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "f2d79083bbffb245d2ce",
-    "url": "/pokedex/static/js/2.b4d591cf.chunk.js"
+    "url": "/static/js/2.b4d591cf.chunk.js"
   },
   {
     "revision": "c72410e3be9613c2dfee",
-    "url": "/pokedex/static/js/3.c0141169.chunk.js"
+    "url": "/static/js/3.c0141169.chunk.js"
   },
   {
     "revision": "6f87069275a0db2af0cd",
-    "url": "/pokedex/static/js/4.5e451764.chunk.js"
+    "url": "/static/js/4.5e451764.chunk.js"
   },
   {
     "revision": "dc7bb9f2c968d627a129",
-    "url": "/pokedex/static/js/main.84e74186.chunk.js"
+    "url": "/static/js/main.84e74186.chunk.js"
   },
   {
     "revision": "f3099af006cce1cf9c0c",
-    "url": "/pokedex/static/js/runtime~main.9a923394.js"
+    "url": "/static/js/runtime~main.9a923394.js"
   }
 ]);
