a01e0b145086d017b44159db32bfb9dfb8451913
Marwa Yahia Galal <engmarwayehya@yahoo.com>
2019-10-17 08:20:39 +0200

    Updates

---
+++ precache-manifest.51c8ab5436a857db24d69c0d5d19ba39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f13b6afa83afa078ff9e879a15fa8d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "f2d79083bbffb245d2ce",
+    "url": "/pokedex/static/js/2.b4d591cf.chunk.js"
+  },
+  {
+    "revision": "250b442815eab3599be4",
+    "url": "/pokedex/static/js/3.e0ce58b6.chunk.js"
+  },
+  {
+    "revision": "6f87069275a0db2af0cd",
+    "url": "/pokedex/static/js/4.5e451764.chunk.js"
+  },
+  {
+    "revision": "04f346bc4c38db62fdd5",
+    "url": "/pokedex/static/js/main.6eac399e.chunk.js"
+  },
+  {
+    "revision": "474edbb8d700e64307d3",
+    "url": "/pokedex/static/js/runtime~main.265ca515.js"
+  }
+]);
