cd2655470f017cad08bc5c83741addc2f204ab8d
Marwa Yahia Galal <engmarwayehya@yahoo.com>
2019-10-17 07:37:12 +0200

    create pokemonCard

---
+++ src/components/PokemonCard/index.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Box, Text } from 'grommet'
+import { ImageStamp } from 'grommet-controls'
+import axios from '../../utils/API'
+import { saveInLocalStorage, getFromLocalStorage } from '../../helper/storage'
+
+const PokemonCard = props => {
+  const { match } = props
+  const { key } = match.params
+  const [pokemonData, setPokemonData] = useState(
+    getFromLocalStorage('pokemonData') || {},
+  )
+
+  useEffect(() => {
+    async function fetchPokemonDataAPI() {
+      const response = await axios(`${key}`)
+      setPokemonData(response.data)
+    }
+    fetchPokemonDataAPI()
+    saveInLocalStorage('pokemonData', pokemonData)
+  }, [key, pokemonData])
+
+  return (
+    <Box
+      border={{ color: 'brand', size: 'small' }}
+      margin={{ horizontal: '15%', bottom: 'medium', top: 'large' }}
+      round="15px"
+      pad="medium"
+      elevation="small"
+      gap="medium"
+      background={{ color: '#ffffff' }}
+    >
+      <Box align="center" margin={{ top: '1em' }}>
+        <ImageStamp
+          src={`${pokemonData.sprites.front_default}`}
+          size="large"
+          round="full"
+        />
+        <Text weight="bold">{pokemonData.name}</Text>
+      </Box>
+      <Text pad={{ top: '1px' }} margin="xsmall">
+        <hr />
+      </Text>
+
+      <Box direction="row" gap="xlarge">
+        <Text weight="bold" color="black">
+          Type:{' '}
+          {pokemonData.types.map(type => (
+            <Text weight="normal" key={type.type.name}>
+              {`${type.type.name}, `}
+            </Text>
+          ))}
+        </Text>
+      </Box>
+      <Box direction="row" gap="xlarge">
+        <Text weight="bold" color="black">
+          Stats:{' '}
+          {pokemonData.stats.map(stat => (
+            <Text weight="normal" key={stat.stat.name}>
+              {`${stat.stat.name}, `}
+            </Text>
+          ))}
+        </Text>
+      </Box>
+    </Box>
+  )
+}
+
+PokemonCard.propTypes = {
+  match: PropTypes.objectOf(PropTypes.any).isRequired,
+}
+
+export default PokemonCard
